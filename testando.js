3625e03386f95a6d3c8ffd36d53c8bbcfa244324
Arthur Almeida <arthurdegois.almeida@gmail.com>
2026-09-19 00:53:17 -0300

    Produção da Home page (em desenvolvimento)

--- testando.js
+++ testando.js
@@ -8,7 +8,7 @@ function verificarLogin() { //Ele está definindo a função verificarLogin sem 
 
     if(usuario === "admin" && senha === "1234") { //Ele está verificando se o usuário é igual a "admin" e a senha é igual a "1234"
         alert("Login bem-sucedido!"); //Se a condição for verdadeira, ele exibirá um alerta com a mensagem "Login bem-sucedido!"
-    window.location.href = "http://localhost/novo_event/Vitrine_eventos.html";
+    window.location.href = "http://localhost/event-hub/home-page.html";
     }
 
     else
@@ -17,3 +17,25 @@ function verificarLogin() { //Ele está definindo a função verificarLogin sem 
     }
 }
 
+// Selecionando os elementos do HTML
+const menuBtn = document.getElementById('menu-btn');
+const closeBtn = document.getElementById('close-btn');
+const sidebar = document.getElementById('sidebar');
+const overlay = document.getElementById('overlay');
+
+// Função para abrir o menu
+function openMenu() {
+    sidebar.classList.add('active');
+    overlay.classList.add('active');
+}
+
+// Função para fechar o menu
+function closeMenu() {
+    sidebar.classList.remove('active');
+    overlay.classList.remove('active');
+}
+
+// Eventos de clique
+menuBtn.addEventListener('click', openMenu); // Abre ao clicar nas 3 barrinhas
+closeBtn.addEventListener('click', closeMenu); // Fecha ao clicar no X
+overlay.addEventListener('click', closeMenu); // Fecha ao clicar fora do menu
